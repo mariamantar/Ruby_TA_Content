07373ef166a6fcd2596f6b96157cf06eab55e845
Mariam <mariam.antar@hotmail.com>
2018-11-10 06:56:06 +1100

    add hashes

--- practice2.rb
+++ practice2.rb
@@ -8,3 +8,21 @@ puts food.sort()
 
 # hashes in ruby
 # a hash stores multiple pieces of data using a key:value pair
+# key:value with a hashrocket in between, mapping the country to the abbreviation
+states = {
+  :NewYork => "NY",
+  "Oregon" => "OR",
+  "pennsylvania" => "PA"
+}
+ puts states["Oregon"]
+
+
+# methods in ruby
+# a block of code that performs a specific task
+# key def means we're defining a method then method name
+# then we passed a parameter which is a value where whoever calls the method is going to give to it
+def say_hi(name)
+  puts "hi #{name}"
+end
+
+say_hi("Mary")
